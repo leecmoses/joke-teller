136446fe27eaa57a355103bfb0cb11df3c06765c
Moses Lee <leecmoses@gmail.com>
2022-05-16 20:02:57 -0400

    Integrate Joke and VoiceRSS apis

--- script.js
+++ script.js
@@ -107,10 +107,14 @@ const VoiceRSS = {
   },
 };
 
-function test() {
+// Disable/Enable Button
+const toggleButton = () => (button.disabled = !button.disabled);
+
+// Passing joke to VoiceRSS API
+const tellMe = (joke) => {
   VoiceRSS.speech({
     key: "c66b95c1563b461385ae1f23890c775e",
-    src: "Hello, world!",
+    src: joke,
     hl: "en-us",
     v: "Linda",
     r: 0,
@@ -118,6 +122,28 @@ function test() {
     f: "44khz_16bit_stereo",
     ssml: false,
   });
+};
+
+// Get Jokes from Joke API
+async function getJokes() {
+  let joke = "";
+  const apiUrl = "https://v2.jokeapi.dev/joke/Programming,Pun";
+  try {
+    const resp = await fetch(apiUrl);
+    const data = await resp.json();
+
+    data.joke ? (joke = data.joke) : (joke = data.setup + " " + data.delivery);
+
+    // Text-to-Speech
+    tellMe(joke);
+    // Disable button
+    toggleButton();
+  } catch (err) {
+    console.log("fetched failed", err);
+  }
 }
 
-test();
+// Event Listeners
+button.addEventListener("click", getJokes);
+
+audioElement.addEventListener("ended", toggleButton);
